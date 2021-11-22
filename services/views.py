77e57c07dfab01cd63cdc3ca54950473073079d0
NelsonEstebanF <esteban1922@hotmail.com>
2021-11-22 11:42:55 -0300

    p

--- services/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Service
-
-def services(request):
-    servicios = Service.objects.all()
-    return render(request, 'services/services.html', { 'servicios': servicios })
