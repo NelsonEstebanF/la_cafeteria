a5dc111f1cdba35fabb39163c10862f4b722b897
NelsonEstebanF <esteban1922@hotmail.com>
2021-11-22 18:55:58 -0300

    add pages

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,11 @@ from .models import Category, Post
 
 def blog(request):
     _all_posts = Post.objects.all()
-    return render(request, 'blog/blog.html', { 'posteos': _all_posts})
-
-#def category(request, category_id ):
-    # Me traigo el objeto category con el id del front
-#    category = get_object_or_404(Category, id=category_id ) # Category.objects.get(id=category_id)
-    # Me traigo los post que tengan esta categoria
-#    _all_posts = Post.objects.filter(categories=category)
-#    return render(request, 'blog/category.html', {'categoria': category, 'posteos': _all_posts })
-
+    context={'title':'Blog', 'posts':_all_posts}
+    return render(request, 'blog/blog.html', context=context)
 
 def category(request, category_id ):
     category = get_object_or_404(Category, id=category_id )
-    return render(request, 'blog/category.html', { 'categoria': category })
+    _all_posts = category.get_posts.all()
+    context={'title':category.name, 'posts':_all_posts}
+    return render(request, 'blog/blog.html', context=context)
